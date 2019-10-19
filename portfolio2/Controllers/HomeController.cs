711ef3a87556aea8e46248470761b223d00b7f04
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-10-19 21:10:06 +0800

    Add project files.

---
+++ portfolio2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace portfolio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //login action method for homepage
+        [HttpPost]
+        public ActionResult Login(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+            if (loginID == "abc@npbook.com" && password == "pass1234")
+            {
+                // Redirect user to the "StaffMain" view through an action
+                return RedirectToAction("VolunteerMain");
+            }
+            else
+            {
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult VolunteerMain()
+        {
+            return View();
+        }
+
+    }
+}
