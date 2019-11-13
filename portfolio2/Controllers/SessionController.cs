0eced5b15384dae9a4dd07d50b7829532f717aa0
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-11-13 10:24:24 +0800

    Migrated Session to SessionController
    Changed all the links from Home - Session Action to Session Controller Index
    
    Usage of correct menu when student is logged in or logged out

---
+++ portfolio2/Controllers/SessionController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using portfolio2.DAL;
+using portfolio2.Models;
+
+namespace portfolio2.Controllers
+{
+    public class SessionController : Controller
+    {
+        private SessionDAL sessionContext = new SessionDAL();
+        private StudentDAL studentContext = new StudentDAL();
+        private LocationDAL locationContext = new LocationDAL();
+        private CategoryDAL categoryContext = new CategoryDAL();
+
+        public IActionResult Index()
+        {
+            List<Session> sessionList = sessionContext.GetAllSessions();
+            List<SessionViewModel> sessionDetailsList = MapToSessionVM(sessionList);
+            return View(sessionDetailsList);
+        }
+        public List<SessionViewModel> MapToSessionVM(List<Session> sessionList)
+        {
+            string studentName = "";
+            string locationName = "";
+            string categoryName = "";
+            List<StudentDetails> studentList = studentContext.GetAllStudent();
+            List<Location> locationList = locationContext.GetAllLocations();
+            List<Category> categoryList = categoryContext.GetAllCategory();
+            List<SessionViewModel> sessionViewModelList = new List<SessionViewModel>();
+            foreach (Session session in sessionList)
+            {
+                foreach (StudentDetails student in studentList)
+                {
+                    if (session.StudentID == student.StudentID)
+                    {
+                        studentName = student.Name;
+                        break;
+                    }
+                }
+                foreach (Location location in locationList)
+                {
+                    if (session.LocationID == location.LocationID)
+                    {
+                        locationName = location.LocationName;
+                        break;
+                    }
+                }
+                foreach (Category category in categoryList)
+                {
+                    if (category.CategoryID == session.CategoryID)
+                    {
+                        categoryName = category.CategoryName;
+                    }
+                }
+                sessionViewModelList.Add(
+                    new SessionViewModel
+                    {
+                        SessionID = session.SessionID,
+                        SessionDate = session.SessionDate,
+                        Name = session.Name,
+                        Description = session.Description,
+                        Photo = session.Photo,
+                        Hours = session.Hours,
+                        Participants = session.Participants,
+                        StudentName = studentName,
+                        LocationName = locationName,
+                        CategoryName = categoryName
+                    });
+            }
+            return sessionViewModelList;
+        }
+
+    }
+}
