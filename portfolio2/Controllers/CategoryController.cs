75b42bb8ecfa885ea607a2e147adf85be6caf27b
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-11-13 13:07:59 +0800

    Added view category page

---
+++ portfolio2/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using portfolio2.Models;
+using portfolio2.DAL;
+
+namespace portfolio2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryDAL categoryContext = new CategoryDAL();
+        public IActionResult Index()
+        {
+            List<Category> categoryList = categoryContext.GetAllCategory();
+            return View(categoryList);
+        }
+    }
+}
