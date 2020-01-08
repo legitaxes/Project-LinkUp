845693e772270ad14aa69754761e12584f6c5a40
Ryan Bangras <ryanbangras@live.com>
2020-01-08 12:45:13 +0800

    Added reference to Microsoft.AspNetCore.WebSockets NuGet package to help in implementing chat functionality

---
+++ portfolio2/Controllers/ChatController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio2.Controllers
+{
+    public class ChatController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
